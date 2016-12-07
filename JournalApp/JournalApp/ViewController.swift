1937587d104ecc35eabc715805eff7cd813f1210
jemalay <jemal@berkeley.edu>
2016-12-07 14:09:14 -0800

    core data commit

--- JournalApp/JournalApp/ViewController.swift
+++ JournalApp/JournalApp/ViewController.swift
@@ -1,22 +1,18 @@
 //
 //  ViewController.swift
-//  Journal
+//  JournalApp
 //
-//  Created by Filip Mirdita on 12/7/16.
-//  Copyright © 2016 Filip Mirdita. All rights reserved.
+//  Created by Jemal Aytjanova on 12/7/16.
+//  Copyright © 2016 Jemal Aytjanova. All rights reserved.
 //
 
 import UIKit
-import CoreData
 
 class ViewController: UIViewController {
-    var isToday = NSDate()
-    @IBOutlet var UIdate: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.UIdate.text = DateFormatter.localizedString(from: isToday as Date, dateStyle: .medium, timeStyle: .medium)
-        
     }
 
     override func didReceiveMemoryWarning() {
