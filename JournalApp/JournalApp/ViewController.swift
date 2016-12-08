2b3f54d1c13aa63d39f6e045c3caec99254a0170
jemalay <jemal@berkeley.edu>
2016-12-07 18:35:48 -0800

    last commit

--- JournalApp/JournalApp/ViewController.swift
+++ JournalApp/JournalApp/ViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     var isToday = NSDate()
+    var answers = [String]()
     @IBOutlet var UIdate: UILabel!
+    @IBAction func UIsubmit(_ sender: Any) {
+        
+        if UIday.text != nil{
+            
+            answers.append(UIday.text!)
+        }
+        if  UImisc.text != nil{
+            answers.append(UImisc.text!)
+        }
+        answers.append("jemal")
+    }
+    
+    @IBOutlet var UIday: UITextField!
+    
+    
+    @IBOutlet var UImisc: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,10 +35,23 @@ class ViewController: UIViewController {
         
         self.UIdate.text = DateFormatter.localizedString(from: isToday as Date, dateStyle: .medium, timeStyle: .medium)
         
-
         
+        if UIday.text != nil{
+            
+            answers.append(UIday.text!)
+        }
+        if  UImisc.text != nil{
+            answers.append(UImisc.text!)
+        }
+        answers.append("jemal")
     }
+    
+    
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
